1c4d4e3829efc047a4cc4cb6dacd9294957c3b3c
Lewis <lewis.alen.day@gmail.com>
2025-07-30 17:22:25 +0100

    Git ignore the vendor fetch content folders - they shouldn't be subject to source control in this repo

--- vendor/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Code/Playing/Minesweeper_Clone/vendor/raylib-src
-# Build directory: E:/Code/Playing/Minesweeper_Clone/vendor/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
